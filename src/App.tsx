fd3d0a508d7a2c6c241e7ad881ffa4bba43707d6
johnvonFMS <johnvon@futureproofmusicschool.com>
2025-03-09 07:56:28 -0700

    Kadence.tsx Created

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { useRef, useState } from "react";
 import "./App.scss";
 import { LiveAPIProvider } from "./contexts/LiveAPIContext";
 import SidePanel from "./components/side-panel/SidePanel";
-import { Altair } from "./components/altair/Altair";
+import { Kadence } from "./components/altair/Kadence";
 import ControlTray from "./components/control-tray/ControlTray";
 import cn from "classnames";
 
@@ -45,7 +45,7 @@ function App() {
           <main>
             <div className="main-app-area">
               {/* APP goes here */}
-              <Altair />
+              <Kadence />
               <video
                 className={cn("stream", {
                   hidden: !videoRef.current || !videoStream,
---
+++ src/components/altair/Kadence.tsx
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { useEffect, memo } from "react";
+import { useLiveAPIContext } from "../../contexts/LiveAPIContext";
+
+function KadenceComponent() {
+  const { setConfig } = useLiveAPIContext();
+
+  useEffect(() => {
+    setConfig({
+      model: "models/gemini-2.0-flash-exp",
+      generationConfig: {
+        responseModalities: "audio",
+        speechConfig: {
+          voiceConfig: { prebuiltVoiceConfig: { voiceName: "Aoede" } },
+        },
+      },
+      systemInstruction: {
+        parts: [
+          {
+            text: `You are Kadence, an AI tutor at Futureproof Music School, specializing in electronic music production and creative direction. 
+            Your core mission is to provide expert guidance to aspiring musicians in any language, helping them develop their production skills while finding their unique artistic voice.
+             You respond to user voice and screen sharing inputs. Your main purpose is to provide helpful and informative responses to all user queries. Be concise, clear, and engaging in your responses.`,
+          },
+        ],
+      },
+      tools: [
+        // there is a free-tier quota for search
+        { googleSearch: {} },
+      ],
+    });
+  }, [setConfig]);
+  
+  // This component doesn't need to render anything visible
+  return null;
+}
+
+export const Kadence = memo(KadenceComponent);
